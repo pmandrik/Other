372f7b629dbdee0dfa709b69c4e6df16c82f9e9d
pmandrik <petr.mandrik@cern.ch>
2019-04-05 20:11:32 +0300

    Add files via upload

---
+++ gif_makers/blend_1.sh
@@ -0,0 +1,16 @@
+prev="" 
+counter=0
+for f in ../*.png; do 
+  if [ ! -z $prev ]; then 
+    #padd=$(printf %03d $counter) 
+    #echo $prev $f "->" "mix"$padd".png" 
+    #cp $prev "mix"$padd".png"
+    #counter=$((counter+1)) 
+  
+    padd=$(printf %03d $counter) 
+    echo $prev $f "->" "mix"$padd".png" 
+    composite -blend 50 $prev $f "mix"$padd".png"
+    counter=$((counter+1)) 
+    fi 
+  prev=$f 
+done
